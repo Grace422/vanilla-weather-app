53aa3f1b438e911085d64ff09d0106f4090363d3
Grace422 <neguekwahammaelgrace@gmail.com>
2023-09-17 20:11:13 +0100

    configured data collected

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ deta.innerHTML = `${jour} ${hours}:${mins}`;
 
 function showCity(response){
     let h1 = document.querySelector('#city');
-    h1.innerHTML = `${response.data.name}`;
+    h1.innerHTML = response.data.name;
     let description = document.querySelector('#description')
-    description.innerHTML = `${response.data.weather[0].description}`
+    description.innerHTML = response.data.weather[0].description;
     let precipitation = document.querySelector('#prep');
-    precipitation.innerHTML = Math.round(`${response.data.main.pressure}`);
+    precipitation.innerHTML = Math.round(response.data.main.pressure);
     let humidity = document.querySelector('#humid');
-    humidity.innerHTML = Math.round(`${response.data.main.humidity}`);
+    humidity.innerHTML = Math.round(response.data.main.humidity);
     let wind = document.querySelector('#wind');
-    wind.innerHTML = Math.round(`${response.data.wind.speed}`);
+    wind.innerHTML = Math.round(response.data.wind.speed);
     let temperature = document.querySelector("#temp");
-    temperature.innerHTML = `${response.data.main.temp}`;
+    temperature.innerHTML = Math.round(response.data.main.temp);
     let iconElement = document.querySelector('#icon');
     iconElement.setAttribute("src",  `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
